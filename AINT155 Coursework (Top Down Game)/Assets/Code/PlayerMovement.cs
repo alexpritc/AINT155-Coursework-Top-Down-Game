afd4213114b4f0c152f4abec7356a5a04191740f
Alex Pritchard <alexpritc@users.noreply.github.com>
2018-11-06 10:31:31 +0000

    Added player movement
    PlayerMovement.cs creates a retro style of movement as it doesn't allow for diagonal/smooth movement. This is intentional in this early stage of Prototyping and can be easily changed in later version to adapt for an improved user experience.

---
+++ AINT155 Coursework (Top Down Game)/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Declaring Variables.
+    public float speed;
+
+    // Called every frame.
+    void Update()
+    {
+        {
+            Movement();
+        }
+    }
+
+    // When the player presses W the character will move in an upward 
+    // direction. When the player presses S the character will move 
+    // in an downward direction. When the player presses A the 
+    // character will move in an leftward direction. When the 
+    // player presses D the character will move in an rightward 
+    // direction. 
+
+    void Movement()
+    {
+        Vector3 position = transform.position;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            Debug.Assert(transform.position == position + (Vector3.up * speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            Debug.Assert(transform.position == position + (Vector3.down * speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            Debug.Assert(transform.position == position + (Vector3.left * speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            Debug.Assert(transform.position == position + (Vector3.right * speed * Time.deltaTime));
+        }
+    }
+}

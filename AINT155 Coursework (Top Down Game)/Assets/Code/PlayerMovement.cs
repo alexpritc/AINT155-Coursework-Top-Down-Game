7d984850db81b59cba2533f37a4c7e6339ea71b4
Alex Pritchard <alexpritc@users.noreply.github.com>
2018-11-09 14:20:27 +0000

    Added Maze Panels, Grates and Animations

--- AINT155 Coursework (Top Down Game)/Assets/Code/PlayerMovement.cs
+++ AINT155 Coursework (Top Down Game)/Assets/Code/PlayerMovement.cs
@@ -7,12 +7,31 @@ public class PlayerMovement : MonoBehaviour
     // Declaring Variables.
     public float speed;
 
+    public bool isSpinning = false;
+
+    Rigidbody2D playerRigidBody;
+
+    Animator playerAnimator;
+
+    // Called at the beginning.
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+
+        playerAnimator.SetBool("isSpinningLeft", false);
+        playerAnimator.SetBool("isSpinningRight", false);
+    }
+
     // Called every frame.
     void Update()
     {
+        // Only allow Player to move when they have no touched a maze panel.
+        if (isSpinning == false)
         {
             Movement();
         }
+        
     }
 
     // When the player presses W the character will move in an upward 
@@ -45,5 +64,70 @@ public class PlayerMovement : MonoBehaviour
             transform.Translate(Vector3.right * speed * Time.deltaTime);
             Debug.Assert(transform.position == position + (Vector3.right * speed * Time.deltaTime));
         }
+    }
+
+    // When Player collides with GameObjects with the following tags...
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // The player starts spinning.
+        if (other.tag == "MazePanelUp")
+        {
+            Invoke("SpinningUp", 0.25f);
+        }
+        if (other.tag == "MazePanelDown")
+        {
+            Invoke("SpinningDown", 0.25f);
+        }
+        if (other.tag == "MazePanelLeft")
+        {
+            Invoke("SpinningLeft", 0.25f);
+        }
+        if (other.tag == "MazePanelRight")
+        {
+            Invoke("SpinningRight", 0.25f);
+        }
+
+        // The player stops spinning.
+        if (other.tag == "Grate")
+        {
+            Invoke("StopSpinning", 0.1f);
+        }
+    }
+
+    // Creates movement when the player hits a Maze Panel.
+    public void SpinningUp()
+    {
+        isSpinning = true;
+        playerRigidBody.velocity = (Vector3.up * speed);
+    }
+    public void SpinningDown()
+    {
+        isSpinning = true;
+        playerRigidBody.velocity = (Vector3.down * speed);
+    }
+    public void SpinningLeft()
+    {
+        isSpinning = true;
+        playerRigidBody.velocity = (Vector3.left * speed);
+
+        playerAnimator.SetBool("isSpinningLeft", true);
+    }
+    public void SpinningRight()
+    {
+        isSpinning = true;
+        playerRigidBody.velocity = (Vector3.right * speed);
+
+        playerAnimator.SetBool("isSpinningRight", true);
+    }
+
+    // Stops the player's current velocity when they collide
+    // with a Grate.
+    public void StopSpinning()
+    {
+        isSpinning = false;
+        playerRigidBody.velocity = (Vector3.zero);
+
+        playerAnimator.SetBool("isSpinningLeft", false);
+        playerAnimator.SetBool("isSpinningRight", false);
     }
 }

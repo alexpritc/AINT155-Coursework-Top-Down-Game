619298601867b6bc5d647d7c3978f51af63247a1
Alex Pritchard <alexpritc@users.noreply.github.com>
2018-11-13 15:52:00 +0000

    Second attempt at fixing Main Menu "player" animation
    Currently: unsuccessful.

---
+++ AINT155 Coursework (Top Down Game)/Assets/Code/MainMenuAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuAnimation : MonoBehaviour {
+
+    // Delcaring variables.
+    Animator timAnimator;
+    bool isLeft = true;
+    bool isRight = false;
+
+    // Called at the beginning on the program.
+    void Start()
+    {
+
+        timAnimator = GetComponent<Animator>();
+    }
+
+    // Called once every fame.
+    void Update()
+    {
+        PlayAnimationMovement();
+    }
+
+    public void PlayAnimationMovement()
+    {
+        if (this.timAnimator.GetCurrentAnimatorStateInfo(0).IsName("Tim_Left"))
+        {
+            isLeft = true;
+            isRight = false;
+        }
+        else
+        { 
+            isLeft = false;
+            isRight = true;
+        }
+
+        PlayAnimationSpinning();       
+    }
+
+    public void PlayAnimationSpinning()
+    {
+
+        if (isLeft == true)
+        {
+            timAnimator.SetBool("isLeft", true);
+        }
+        else
+        {
+            timAnimator.SetBool("isRight", true);
+        }
+
+    }
+
+}

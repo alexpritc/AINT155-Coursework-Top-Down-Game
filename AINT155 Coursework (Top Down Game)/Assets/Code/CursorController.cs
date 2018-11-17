91b0f7ab99843615475cffb65b5715715ccbb751
Alex Pritchard <alexpritc@users.noreply.github.com>
2018-11-17 00:02:21 +0000

    Hidden Cursor

---
+++ AINT155 Coursework (Top Down Game)/Assets/Code/CursorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        // Hides the cursor in-game.
+        Cursor.visible = false;
+
+	}
+	
+}

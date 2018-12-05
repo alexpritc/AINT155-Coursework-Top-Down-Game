7bc2c948fcd54a9a9e99484a8f9dad10e255d47b
Alex Pritchard <alexpritc@users.noreply.github.com>
2018-12-05 18:40:25 +0000

    Added level 1.5
    Level 1.5 introduces obstacles to the player.

--- AINT155 Coursework (Top Down Game)/Assets/Scripts/DialogueTrigger.cs
+++ AINT155 Coursework (Top Down Game)/Assets/Scripts/DialogueTrigger.cs
@@ -15,15 +15,22 @@ public class DialogueTrigger : MonoBehaviour {
 
     public float timer = 0f;
 
+    // Level 1 triggers.
     bool hasLevel1Trigger1BeenCalled = false;
     bool hasLevel1Trigger2BeenCalled = false;
     bool hasLevel1Trigger3BeenCalled = false;
+
+    // Level 1.5 triggers.
+    bool hasLevel1AndAHalfTrigger1BeenCalled = false;
 
 
     // Called once at initialisation.
     void Start()
     {
-        stairsToNextLevel.SetActive(false);
+        if (sceneName == "Level1")
+        {
+            stairsToNextLevel.SetActive(false);
+        }
     }
 
     // Called once every frame.
@@ -41,54 +48,94 @@ public class DialogueTrigger : MonoBehaviour {
     {
         if (sceneName == "Level1")
         {
-            if (hasLevel1Trigger1BeenCalled == false)
-            {
-                Invoke("TriggerDialogue", 1f);
-
-                currentDialogue = "WASD";
-                hasLevel1Trigger1BeenCalled = true;
-
-            }
-
-            // N/A says something whilst the Player is moving.
-            if (timer >= 5f && currentDialogue == "WASD" && hasLevel1Trigger1BeenCalled == true)
-            {
-                dialogue.sentences[0] = "This is good. Keep going.";
-                dialogue.sentences[1] = "Excellent.";
-
-                Invoke("TriggerDialogue", 2f);
-
-                currentDialogue = "Next Level";
-                hasLevel1Trigger2BeenCalled = true;
-
-            }
-
-            // N/A says something to indicate the player
-            // needs to go to the next Level.
-            if (timer >= 15f && currentDialogue == "Next Level" && hasLevel1Trigger2BeenCalled == true)
-            {
-
-                dialogue.sentences[0] = "Well done, Tim. Now, I think we should talk more.";
-                dialogue.sentences[1] = "Walk into the stairs to explore deeper.";
-
-                Invoke("TriggerDialogue", 1f);
-
-                currentDialogue = "End Of Level";
-                hasLevel1Trigger3BeenCalled = true;
-
-                if (hasLevel1Trigger3BeenCalled == true)
-                {
-                    stairsToNextLevel.SetActive(true);
-                }
-
-            }
+            Level1();
         }
 
+        if (sceneName == "Level1.5")
+        {
+            Level1AndAHalf();
+        }
+
+    }
+
+    // Level 1.
+    public void Level1()
+    {
+        if (hasLevel1Trigger1BeenCalled == false)
+        {
+            Invoke("TriggerDialogue", 1f);
+
+            currentDialogue = "WASD";
+            hasLevel1Trigger1BeenCalled = true;
+
+        }
+
+        // N/A says something whilst the Player is moving.
+        if (timer >= 5f && currentDialogue == "WASD" && hasLevel1Trigger1BeenCalled == true)
+        {
+            dialogue.sentences[0] = "This is good. Keep going.";
+            dialogue.sentences[1] = "Excellent.";
+
+            Invoke("TriggerDialogue", 2f);
+
+            currentDialogue = "Next Level";
+            hasLevel1Trigger2BeenCalled = true;
+
+        }
+
+        // N/A says something to indicate the player
+        // needs to go to the next Level.
+        if (timer >= 15f && currentDialogue == "Next Level" && hasLevel1Trigger2BeenCalled == true)
+        {
+
+            dialogue.sentences[0] = "Well done, Tim. Now, I think we should talk more.";
+            dialogue.sentences[1] = "Walk into the stairs to explore deeper.";
+
+            Invoke("TriggerDialogue", 1f);
+
+            currentDialogue = "End Of Level";
+            hasLevel1Trigger3BeenCalled = true;
+
+            if (hasLevel1Trigger3BeenCalled == true)
+            {
+                stairsToNextLevel.SetActive(true);
+            }
+
+        }
+    }
+
+    // Level 1.5.
+    public void Level1AndAHalf()
+    {
+        if (hasLevel1AndAHalfTrigger1BeenCalled == false)
+        {
+            Invoke("TriggerDialogue", 0.5f);
+            hasLevel1AndAHalfTrigger1BeenCalled = true;
+        }
     }
 
     // Triggers dialogue.
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == ("Player"))
+        {
+            if (sceneName == "Level1.5")
+            {
+                dialogue.sentences[0] = "Well that wasn't very smart.";
+
+                if (sceneName == "Level1.5" & hasLevel1AndAHalfTrigger1BeenCalled == true)
+                {
+                    dialogue.sentences[0] = "What did I just say?";
+                }
+
+                Invoke("TriggerDialogue", 0.5f);
+            }
+        }
+       
     }
 }

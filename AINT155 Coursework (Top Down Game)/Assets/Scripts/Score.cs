3e7de43d4251317e967895f1c332a48f929cddf0
Alex Pritchard <alexpritc@users.noreply.github.com>
2019-01-14 00:03:57 +0000

    Added percentages to game over screen

--- AINT155 Coursework (Top Down Game)/Assets/Scripts/Score.cs
+++ AINT155 Coursework (Top Down Game)/Assets/Scripts/Score.cs
@@ -15,14 +15,66 @@ public class Score : MonoBehaviour {
         switch (SceneController.sceneNameGameOver)
         {
             case "Level1.5":
-                percentage = "10%";
+                percentage = "5%";
                 break;
             case "Level2":
-                percentage = "20%";
+                percentage = "10%";
                 break;
             case "Level3":
+                percentage = "15%";
+                break;
+            case "Level4":
+                percentage = "20%";
+                break;
+            case "Level5":
+                percentage = "25%";
+                break;
+            case "Level6":
                 percentage = "30%";
                 break;
+            case "Level7":
+                percentage = "35%";
+                break;
+            case "Level8":
+                percentage = "40%";
+                break;
+            case "Level9":
+                percentage = "45%";
+                break;
+            case "Level10":
+                percentage = "50%";
+                break;
+            case "Level11":
+                percentage = "55%";
+                break;
+            case "Level12":
+                percentage = "60%";
+                break;
+            case "Level13":
+                percentage = "65%";
+                break;
+            case "Level14":
+                percentage = "70%";
+                break;
+            case "Level15":
+                percentage = "75%";
+                break;
+            case "Level16":
+                percentage = "80%";
+                break;
+            case "Level17":
+                percentage = "85%";
+                break;
+            case "Level18":
+                percentage = "90%";
+                break;
+            case "Level19":
+                percentage = "95%";
+                break;
+            case "Level20":
+                percentage = "99%";
+                break;
+
         }
 
         displayScore.text = percentage;
